99daf7e1528853aa0020b58f8f5d9f86e88a738a
Sachin Meshram <138348581+Sachin9548@users.noreply.github.com>
2025-07-11 12:09:21 +0530

    Add files via upload

--- src/components/Data/ProjectPage.js
+++ src/components/Data/ProjectPage.js
@@ -1,6 +1,10 @@
+import Chatapp from '../../assets/Chatapp.png';           
+import DataGram from '../../assets/DataGram.png';           
+import FileSender from '../../assets/FileSender.png';  
+
 export const ProjectData=[   
   {
-    url:"https://cdn.glitch.global/5b6dafa4-b7dc-40e2-a341-b1310b1067cd/Screenshot_20240405_121701.png?v=1712299805219",
+    url:Chatapp,
     name:"Chat-Ingo",
     subname:"Chat App",
     detail:" Developed a **real-time chat application** using (MERN stack), and Socket.IO. The application allows users to send and receive messages in real-time, enhancing the user experience and interaction. Implemented user authentication for secure access by using JWT and bcrypt.js",
@@ -10,7 +14,7 @@ export const ProjectData=[
     
   },
    {
-    url:"https://cdn.glitch.global/5b6dafa4-b7dc-40e2-a341-b1310b1067cd/Screenshot_20240405_121822.png?v=1712299847074",
+    url:DataGram,
     name:"DataGram",
     subname:"Social Media App",
     detail:"  Developed a full-stack social media application that allows users to create profiles, post updates,Like and dislike post, and interact with other users. The application supports real-time updates and has an intuitive user interface. Successfully implemented real-time updates and Optimized application performance by 20% through user friendly UI",
@@ -20,7 +24,7 @@ export const ProjectData=[
     
   },
    {
-    url:"https://cdn.glitch.global/5b6dafa4-b7dc-40e2-a341-b1310b1067cd/Screenshot_20240407_030418.png?v=1712561492560",
+    url:FileSender,
     name:"File Sender",
     subname:"File sharing app",
     detail:" The primary purpose of this application is to facilitate the easy and secure sharing of files among users. It allows users to upload, download, and manage files in a centralized location. Users can upload files to the server and download them when needed.",
--- src/components/Data/React.js
+++ src/components/Data/React.js
@@ -1,6 +1,8 @@
+import Restapitester from '../../assets/Restapitester.png';
+
 export const ReactData=[
   {
-    url:"https://cdn.glitch.global/5b6dafa4-b7dc-40e2-a341-b1310b1067cd/Screenshot_20240404_044843.png?v=1712296402827",
+    url:Restapitester,
     name:"Rest Api Tester",
     subname:"online Api Tester",
     detail:"Users can simulate various types of API requests, including GET,POST, PUT, DELETE , etc. Users can simulate different error scenarios to understand how their APIs behave under various conditions",
